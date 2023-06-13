bd4254bb9e00040ba9be1389ebadabc3ac53ad2f
peter staal <peterstaal@MBP-van-peter.home>
2023-06-13 11:34:44 +0200

    add form submission logic

--- contacts.js
+++ contacts.js
@@ -45,6 +45,7 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("common"));
 
 app.get("/", (req, res) => {
@@ -59,6 +60,11 @@ app.get("/contacts", (req, res) => {
 
 app.get("/contacts/new", (req, res) => {
     res.render("new-contact");
+});
+
+app.post("/contacts/new", (req, res) => {
+    contactData.push({ ...req.body });
+    res.redirect("/contacts");
 });
 
 app.listen(3000, "localhost", () => {
